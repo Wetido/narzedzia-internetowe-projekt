05f254e099baf93708fa20f94fe76a44de47af62
Jakub <wetido@gmail.com>
2019-05-09 22:30:47 +0200

    Zmiana koncepcji projektu. Dodanie animacji.

---
+++ assets/scripts/animacja.js
@@ -0,0 +1,37 @@
+
+window.sr = ScrollReveal();
+sr.reveal('#przesuwanie-o-mnie', {
+    reset: true,
+    delay: 200,
+    distance: 0,
+    duration: 800,
+    distance: '50px',
+    origin: 'left'
+});
+
+sr.reveal('#przesuwanie-kontakt', {
+    reset: true,
+    delay: 200,
+    distance: 0,
+    duration: 800,
+    distance: '50px',
+    origin: 'right'
+});
+
+sr.reveal('.zdjecie', {
+    reset: true,
+    delay: 300,
+    distance: 0,
+    duration: 1200,
+    distance: '50px',
+    origin: 'top'
+});
+
+sr.reveal('.content > p', {
+    reset: true,
+    delay: 200,
+    distance: 0,
+    duration: 800,
+    distance: '50px',
+    origin: 'top'
+});
